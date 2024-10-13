fcd74a7457035afaa9a88d9fa241a88da5710128
mnmhbbb <mnmhb64@gmail.com>
2024-10-13 22:39:37 +0900

    chore: 불필요한 코드 삭제

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -12,33 +12,6 @@ export interface PostModel {
   commentCount: number;
 }
 
-const posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'John',
-    title: 'John Smith',
-    content: 'hello',
-    likeCount: 1,
-    commentCount: 1,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '인사하는 민지',
-    likeCount: 10000000,
-    commentCount: 99999,
-  },
-  {
-    id: 3,
-    author: 'viviz_official',
-    title: '고양이 신비',
-    content: '신비는 냥',
-    likeCount: 123456,
-    commentCount: 12345,
-  },
-];
-
 @Injectable()
 export class PostsService {
   constructor(

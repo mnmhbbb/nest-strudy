3fd4cf9bbfe1ce20959b8e95ff4b0bbb15efd9c4
mnmhbbb <mnmhb64@gmail.com>
2024-10-24 00:23:56 +0900

    feat: 비밀번호 커스텀 파이프 추가2

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -7,10 +7,33 @@ import {
 
 @Injectable()
 export class PasswordPipe implements PipeTransform {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value: any, metadata: ArgumentMetadata) {
     if (value.toString().length > 8) {
       throw new BadRequestException('8자 이하로 입력해 주세요!');
+    }
+
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MaxLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length > this.length) {
+      throw new BadRequestException(`최대 길이는 ${this.length}입니다`);
+    }
+
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MinLengthPipe implements PipeTransform {
+  constructor(private readonly length: number) {}
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length < this.length) {
+      throw new BadRequestException(`최소 길이는 ${this.length}입니다`);
     }
 
     return value.toString();
